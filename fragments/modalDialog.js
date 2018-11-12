d2b6cd7c3adaeaaed6fee8525c02ca4bfb44a9f9
Sergey Podguzov <spo@virtoway.com>
2018-11-12 16:12:02 +0500

    E2E: Merging cart critical path. Some refactoring using signIn function.

--- fragments/modalDialog.js
+++ fragments/modalDialog.js
@@ -1,15 +1,15 @@
 const I = actor();
 
 module.exports = {
-
-    // insert your locators and methods here
-    modalDialog:
-        {
-            modalWIndow: "//div[contains(@class, 'modal-dialog')]",
-            goToCardButton: "//button[@class='btn'][1]",
-            viewYourListButton: '//button/span[contains(., "View your list")]',
-            pleaseSelectList: {
-                addToList: '//button[@ng-click="addProductToList()"]'
-            }
+    modalDialog: {
+        modalWIndow: "//div[contains(@class, 'modal-dialog')]",
+        goToCardButton: "//button[@class='btn'][1]",
+        viewYourListButton: '//button/span[contains(., "View your list")]',
+        pleaseSelectList: {
+            addToList: '//button[@ng-click="addProductToList()"]'
+        },
+        productIsAddedToCart: {
+            goToCartButton: '//button/span[contains(., "Go to cart")]'
         }
-}
+    }
+};
---
+++ tests/e2eMergingCart_test.js
@@ -0,0 +1,23 @@
+/// <reference path="../steps.d.ts" />
+
+Feature('Merging Cart');
+
+Before((I, mergingCart, modalDialogFragment) => {
+    I.amOnPage('');
+
+    I.scrollPageToBottom();
+    I.click(mergingCart.firstProductAddToCartButton);
+    I.click(modalDialogFragment.modalDialog.productIsAddedToCart.goToCartButton);
+
+    I.seeElement(mergingCart.shoppingCartTitle);
+    I.seeElement(mergingCart.cartItem);
+
+    I.signIn();
+});
+
+Scenario('[Positive] First step', async (I, mergingCart) => {
+    I.waitForVisible(mergingCart.cartButton);
+    I.click(mergingCart.cartButton);
+    I.seeElementInDOM(mergingCart.authorizedUserMark);
+    I.seeElement(mergingCart.cartItem);
+});
