a55b6f3b02c1b76a0261316eae2b370e206be687
Sergey Podguzov <spo@virtoway.com>
2018-11-15 16:33:55 +0500

    E2E: Two tests: Search by keyword + Product compare

---
+++ tests/e2eSearchByKeyword_test.js
@@ -0,0 +1,20 @@
+/// <reference path="../steps.d.ts" />
+
+Feature('Search By Keyword');
+
+Scenario('[Positive] Search category and product by criteria', async (I, search) => {
+    I.amOnPage('');
+    I.fillField(search.criteria, 'sony');
+    I.seeElementInDOM(search.results);
+
+    var firstFoundCategoryText = await I.grabTextFrom(search.firstFoundCategory);
+    I.click(search.firstFoundCategory);
+    I.see(firstFoundCategoryText.toUpperCase(), search.foundCategoryResult);
+
+    I.fillField(search.criteria, 'sony');
+    I.seeElementInDOM(search.results);
+
+    var firstFoundProductText = await I.grabTextFrom(search.firstFoundProduct);
+    I.click(search.firstFoundProduct);
+    I.see(firstFoundProductText.toUpperCase(), search.foundProductResult);
+});
