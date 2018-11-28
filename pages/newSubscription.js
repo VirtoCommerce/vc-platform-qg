69c69af9c9e8dc1ac5e394d663ab394c21f2fdea
Sergey Podguzov <spo@virtoway.com>
2018-11-28 17:30:52 +0500

    E2E: Changing language and currency + new subscription create.

---
+++ pages/newSubscription.js
@@ -0,0 +1,38 @@
+const I = actor();
+
+module.exports = {
+    product: '//*[@id="Electronics"]/main/div[4]/div[4]/a',
+    addToCartButton: '//button[@id="addToCart"]',
+    shoppingCart: {
+        item: '//form[@name="formCart"]/div[contains(@class, "cart-row")][contains(@ng-repeat, "cart")]',
+        checkOutButton: '//button[@ng-click="submitCart()"]'
+    },
+    customer: {
+        email: '//input[@id="Email"]',
+        firstName: '//input[@id="FirstName"]',
+        lastName: '//input[@id="LastName"]',
+        company: '//input[@id="Organization"]',
+        address: '//input[@id="Line1"]',
+        city: '//input[@id="City"]',
+        country: '//select[@name="Country"]/option[2]',
+        region: '//select[@name="Region"]/option[2]',
+        postalCode: '//input[@id="PostalCode"]',
+        iWantThisToBeRecurringOrder: '//div[@class="switch"]',
+        autoReorderType: {
+            everyDay: '//select[@id="pplan"]/option[1]'
+        },
+        shippingMethodButton: '//button/span[text()="Shipping method"]/..'
+    },
+    shippingMethods: {
+        fixedRateGround: '//label[@for="FixedRate:Ground"]'
+    },
+    paymentMethodButton: '//button/span[text()="Payment method"]/..',
+    createOrderButton: '//button/span[text()="Create order"]/..',
+    subscriptionInfo: '//table[@class="vc-product-table"]/tbody/tr[contains(@class, "product-row")]/td/div/span',
+    myAccountInsets: {
+        orders: '//ul[contains(@class, "nav-tabs")]/li/a[text()="Orders"]',
+        subscriptions: '//ul[contains(@class, "nav-tabs")]/li/a[text()="Subscriptions"]'
+    },
+    subscriptionsList: '//table[contains(@class, "full")]',
+    created: '//table/tbody/tr[1]/td[1]/a'
+};
---
+++ tests/e2e_MultiCurrencyMultiLanguage_test.js
@@ -0,0 +1,31 @@
+/// <reference path="../steps.d.ts" />
+
+Feature('Multi currency multi language');
+
+Scenario('[Positive] Change language and currency', async (I, storeOptions) => {
+    I.amOnPage('');
+    await I.moveCursorTo(storeOptions.selector);
+    await I.moveCursorTo(storeOptions.language);
+    await I.click(storeOptions.deutsch);
+    await I.click(storeOptions.firstProduct);
+
+    var brandDE = await I.grabTextFrom(storeOptions.productDetails.brand);
+    var displaySizeDE = await I.grabTextFrom(storeOptions.productDetails.displaySize);
+    var weAlsoRecommendDE = await I.grabTextFrom(storeOptions.productDetails.weAlsoRecommend);
+
+    I.waitForVisible(storeOptions.productDetails.comparePrice, 10);
+    var comparePriceDE = await I.grabTextFrom(storeOptions.productDetails.comparePrice);
+
+    var assert = require('assert');
+    assert.equal(brandDE, storeOptions.productDetails.brandDE);
+    assert.equal(displaySizeDE, storeOptions.productDetails.displaySizeDE);
+    assert.equal(weAlsoRecommendDE, storeOptions.productDetails.weAlsoRecommendDE);
+    assert.ok(comparePriceDE.indexOf(storeOptions.productDetails.comparePriceDE) > -1);
+
+    await I.moveCursorTo(storeOptions.storeSpan);
+    await I.moveCursorTo(storeOptions.currency.eur.selector);
+    await I.click(storeOptions.firstProduct);
+
+    var price = await I.grabTextFrom(storeOptions.productDetails.price);
+    assert.ok(price.indexOf(storeOptions.currency.eur.sign) > -1);
+});
