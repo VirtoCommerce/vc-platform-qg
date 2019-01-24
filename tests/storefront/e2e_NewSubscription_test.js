ed1eb0d0094b697f969f00fc766bba9c651494b1
Sergey Podguzov <spo@virtoway.com>
2019-01-24 15:04:55 +0500

    E2E: BulkOrder and NewSubscription tests fixes.

--- tests/storefront/e2e_NewSubscription_test.js
+++ tests/storefront/e2e_NewSubscription_test.js
@@ -5,46 +5,56 @@ Feature('New subscription');
 Scenario('[Positive] Create', async (I, newSubscription, modalDialogFragment) => {
     I.amOnPage('');
     I.signIn();
-    
-    I.click(newSubscription.product);
-    I.scrollPageToBottom();
-    I.click(newSubscription.addToCartButton);
 
-    I.waitForClickable(modalDialogFragment.modalDialog.productIsAddedToCart.goToCartButton, 15);
-    I.click(modalDialogFragment.modalDialog.productIsAddedToCart.goToCartButton);
-    
+    await I.click(newSubscription.product);
+    I.scrollPageToBottom();
+    I.wait(15);
+    //I.waitForElement(newSubscription.addToCartButton, 15);
+    await I.click(newSubscription.addToCartButton);
+
+    I.wait(15);
+    //I.waitForEnabled(modalDialogFragment.modalDialog.productIsAddedToCart.goToCartButton, 15);
+    await I.click(modalDialogFragment.modalDialog.productIsAddedToCart.goToCartButton);
+
     I.seeElementInDOM(newSubscription.shoppingCart.item);
     I.scrollPageToBottom();
-    I.click(newSubscription.shoppingCart.checkOutButton);
+    await I.click(newSubscription.shoppingCart.checkOutButton);
 
     var c = newSubscription.customer;
+    I.wait(10);
     I.fillField(c.email, I.customerInfo.email);
     I.fillField(c.firstName, I.customerInfo.firstName);
     I.fillField(c.lastName, I.customerInfo.lastName);
     I.fillField(c.company, I.customerInfo.company);
     I.fillField(c.address, I.customerInfo.address);
     I.fillField(c.city, I.customerInfo.city);
-    I.click(c.country);
-    I.click(c.region);
+    await I.click(c.country);
+    await I.click(c.region);
     I.fillField(c.postalCode, I.customerInfo.postalCode);
-    
-    I.click(c.iWantThisToBeRecurringOrder);
 
-    await I.waitForElement(c.autoReorderType.everyDay, 10);
-    I.click(c.autoReorderType.everyDay);
+    I.waitForElement(c.iWantThisToBeRecurringOrder, 10);
+    await I.click(c.iWantThisToBeRecurringOrder);
+
+    I.wait(15);
+    //I.waitForElement(c.autoReorderType.everyDay, 10);
+    await I.click(c.autoReorderType.everyDay);
     I.scrollPageToBottom();
-    var info = await I.grabTextFrom(newSubscription.subscriptionInfo);
-    await I.waitForClickable(c.shippingMethodButton, 15);
+    I.wait(15);
+    //await I.waitForEnabled(c.shippingMethodButton, 15);
     await I.click(c.shippingMethodButton);
 
     await I.click(newSubscription.shippingMethods.fixedRateGround);
-    await I.waitForClickable(newSubscription.paymentMethodButton, 15);
+    I.wait(15);
+    //await I.waitForEnabled(newSubscription.paymentMethodButton, 15);
     await I.click(newSubscription.paymentMethodButton);
-    
-    await I.waitForClickable(newSubscription.createOrderButton, 15);
-    I.click(newSubscription.createOrderButton);
 
-    I.click(newSubscription.myAccountInsets.subscriptions);
-    await I.waitForElement(newSubscription.subscriptionsList);
-    I.click(newSubscription.created);
+    I.wait(15);
+    //await I.waitForEnabled(newSubscription.createOrderButton, 15);
+    await I.click(newSubscription.createOrderButton);
+
+    I.wait(15);
+    await I.click(newSubscription.myAccountInsets.subscriptions);
+    I.wait(15);
+    //await I.waitForElement(newSubscription.subscriptionsList, 15);
+    await I.click(newSubscription.created);
 });
