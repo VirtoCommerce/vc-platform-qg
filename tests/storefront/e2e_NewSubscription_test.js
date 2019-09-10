342bd5e9ef9bbc9646d966b51e98f78d440ebd40
Denis Uvarenkov <DIUvarenkov@grotem.com>
2019-09-10 16:52:53 +0300

    VP-443. Fixed NewSubscription_test

--- tests/storefront/e2e_NewSubscription_test.js
+++ tests/storefront/e2e_NewSubscription_test.js
@@ -6,56 +6,67 @@ Scenario('[Positive] Create', async (I, newSubscription, modalDialogFragment) =>
     I.amOnPage('');
     I.signIn();
 
-    await I.click(newSubscription.product);
-    I.scrollPageToBottom();
-    I.wait(15);
-    //I.waitForElement(newSubscription.addToCartButton, 15);
+    //click on product
+    I.waitForElement(newSubscription.product, 5);
+    I.pressKey("Pagedown");
+
+    I.waitForElement(newSubscription.product);
+    I.click(newSubscription.product);
     await I.click(newSubscription.addToCartButton);
 
-    I.wait(15);
-    //I.waitForEnabled(modalDialogFragment.modalDialog.productIsAddedToCart.goToCartButton, 15);
-    await I.click(modalDialogFragment.modalDialog.productIsAddedToCart.goToCartButton);
+    I.waitForElement(modalDialogFragment.modalDialog.productIsAddedToCart.goToCartButton, 5);
+    I.click(modalDialogFragment.modalDialog.productIsAddedToCart.goToCartButton);
 
-    I.wait(15);
-    I.seeElementInDOM(newSubscription.shoppingCart.item);
+    I.seeInCurrentUrl("/cart");
     I.scrollPageToBottom();
-    await I.click(newSubscription.shoppingCart.checkOutButton);
+    I.waitForElement(newSubscription.shoppingCart.item, 5);
+
+    I.waitForEnabled(newSubscription.shoppingCart.checkOutButton,5);
+    I.click(newSubscription.shoppingCart.checkOutButton);
+
+    I.wait(3);
+    I.seeInCurrentUrl("/checkout");
 
     var c = newSubscription.customer;
-    I.wait(10);
+    I.waitForElement(c.email, 10);
     I.fillField(c.email, I.customerInfo.email);
     I.fillField(c.firstName, I.customerInfo.firstName);
     I.fillField(c.lastName, I.customerInfo.lastName);
     I.fillField(c.company, I.customerInfo.company);
     I.fillField(c.address, I.customerInfo.address);
     I.fillField(c.city, I.customerInfo.city);
-    await I.click(c.country);
-    await I.click(c.region);
+
+    I.click(c.country);
+
+    I.waitForEnabled(c.region, 10);
+    I.click(c.region);
+
+    I.waitForEnabled(c.postalCode, 10);
     I.fillField(c.postalCode, I.customerInfo.postalCode);
 
-    I.waitForElement(c.iWantThisToBeRecurringOrder, 10);
-    await I.click(c.iWantThisToBeRecurringOrder);
+    I.waitForEnabled(c.iWantThisToBeRecurringOrder, 10);
+    I.click(c.iWantThisToBeRecurringOrder);
 
-    I.wait(15);
-    //I.waitForElement(c.autoReorderType.everyDay, 10);
-    await I.click(c.autoReorderType.everyDay);
+    I.waitForEnabled(c.autoReorderType.everyDay, 10);
+    I.click(c.autoReorderType.everyDay);
+
     I.scrollPageToBottom();
-    I.wait(15);
-    //await I.waitForEnabled(c.shippingMethodButton, 15);
-    await I.click(c.shippingMethodButton);
+    I.wait(3);
+    I.click(c.shippingMethodButton);
 
-    await I.click(newSubscription.shippingMethods.fixedRateGround);
-    I.wait(15);
-    //await I.waitForEnabled(newSubscription.paymentMethodButton, 15);
-    await I.click(newSubscription.paymentMethodButton);
+    I.waitForElement(newSubscription.shippingMethods.fixedRateGround, 10);
+    I.click(newSubscription.shippingMethods.fixedRateGround);
 
-    I.wait(15);
-    //await I.waitForEnabled(newSubscription.createOrderButton, 15);
-    await I.click(newSubscription.createOrderButton);
+    I.waitForEnabled(newSubscription.paymentMethodButton, 10);
+    I.click(newSubscription.paymentMethodButton);
 
-    I.wait(15);
-    await I.click(newSubscription.myAccountInsets.subscriptions);
-    I.wait(15);
-    //await I.waitForElement(newSubscription.subscriptionsList, 15);
-    await I.click(newSubscription.created);
+    I.wait(5);
+    I.waitForEnabled(newSubscription.createOrderButton, 10);
+    I.click(newSubscription.createOrderButton);
+
+    I.waitForElement(newSubscription.myAccountInsets.subscriptions, 10);
+    I.click(newSubscription.myAccountInsets.subscriptions);
+
+    I.waitForElement(newSubscription.subscriptionsList, 10);
+    I.click(newSubscription.created);
 });
