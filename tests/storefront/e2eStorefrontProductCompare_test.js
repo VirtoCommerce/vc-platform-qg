ea9840575d0dc50f693cf21a44ddf4c8b674f274
Denis Uvarenkov <DIUvarenkov@grotem.com>
2019-09-12 15:19:40 +0300

    Fixed productCompare test. Removed wait functions

--- tests/storefront/e2eStorefrontProductCompare_test.js
+++ tests/storefront/e2eStorefrontProductCompare_test.js
@@ -4,22 +4,24 @@ Feature('Storefront Product Compare');
 
 Scenario('[Positive] Compare two products', async (I, productCompare, modalDialogFragment) => {
     I.amOnPage('');
-    
+    I.pressKey("Pagedown");
+    I.waitForEnabled(productCompare.firstProduct);
     I.click(productCompare.firstProduct);
-    I.wait(15);
     //I.waitForClickable(productCompare.addToCompareListButton);
     I.scrollPageToBottom();
     I.click(productCompare.addToCompareListButton);
     I.click(modalDialogFragment.modalDialog.compareList.continueShoppingButton);
 
     I.click(productCompare.headPhonesInset);
-    
+
+    I.pressKey("Pagedown");
+    I.waitForEnabled(productCompare.secondProduct);
     I.click(productCompare.secondProduct);
-    I.wait(15);
+
     I.click(productCompare.addToCompareListButton);
     I.scrollPageToBottom();
     I.click(modalDialogFragment.modalDialog.compareList.goToCompareListButton);
 
-    I.wait(15);
+    I.waitForElement(productCompare.compareToolbar);
     I.seeElementInDOM(productCompare.compareToolbar);
 });
